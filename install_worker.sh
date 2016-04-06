846318ae25180ea32c730ac2a096f082f1b1b04e
dhoppe83 <hoppe@hlrs.de>
2016-04-06 17:16:28 +0200

    Update install_worker.sh

--- install_worker.sh
+++ install_worker.sh
@@ -3,7 +3,6 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0
 source /usr/local/rvm/scripts/rvm
 
-echo "Check if env variable exists: " $PUBLIC_RabbitMQ_Required_by_Worker
 sed -i -e "s|amqp://localhost|amqp://$PUBLIC_RabbitMQ_Required_by_Worker|" attributes/hyperflow-md-worker.json
 echo amqp://$PUBLIC_RabbitMQ_Required_by_Worker >> AMQP_URL
 bash install.sh attributes/hyperflow-md-worker.json
