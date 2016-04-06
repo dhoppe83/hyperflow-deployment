7fc1731f4cae71660f0ecb34f00f31c400aa1907
dhoppe83 <hoppe@hlrs.de>
2016-04-06 17:26:17 +0200

    Create start_master_with_docker.sh

---
+++ start_master_with_docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+env WORKDIR=/tmp AMQP_URL=$(cat /hyperflow-deployment/AMQP_URL) REDIS_IP=$(cat /hyperflow-deployment/REDIS_IP) REDIS_PORT=6379 /node_modules/hyperflow/bin/hflow run -s md_amqp.json
