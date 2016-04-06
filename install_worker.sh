2926e61bbf28c855d8fd77dc4c8e89dd84d972f5
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-04-06 10:57:49 +0200

    +x

---
+++ install_worker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0
+source /usr/local/rvm/scripts/rvm
+
+echo "Check if env variable exists: " $PUBLIC_RabbitMQ_Required_by_Worker
+cd hyperflow-deployment/
+sed -i -e "s|amqp://localhost|amqp://$PUBLIC_RabbitMQ_Required_by_Worker|" attributes/hyperflow-md-worker.json
+install.sh attributes/hyperflow-md-worker.json
