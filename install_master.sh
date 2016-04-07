3695b21b3e38afb5ed8545dd1e97e894c9271529
dhoppe83 <hoppe@hlrs.de>
2016-04-07 14:20:51 +0200

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -5,5 +5,5 @@ curl -o /hyperflow-deployment/md_amqp.json https://raw.githubusercontent.com/dic
 
 echo amqp://$PUBLIC_RabbitMQ_Required_by_Master > AMQP_URL
 REDIS_URL=$PUBLIC_Redis_Required_by_Master
-URL_ARRAY=(${AMQP_URL//:/ })
+URL_ARRAY=(${REDIS_URL//:/ })
 echo "${URL_ARRAY[1]}" > REDIS_IP
