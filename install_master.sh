61f43caecf823fcadde9261686b31ea6b8333cce
dhoppe83 <hoppe@hlrs.de>
2016-04-07 10:11:15 +0200

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -o /hyperflow-deployment/md_amqp.json https://raw.githubusercontent.com/dice-cyfronet/hyperflow/develop/examples/MolecularDynamicsParameterStudy/md_amqp.json
 
-echo amqp://$PUBLIC_RabbitMQ_Required_by_Master >> AMQP_URL
+echo amqp://$PUBLIC_RabbitMQ_Required_by_Master > AMQP_URL
 REDIS_URL=$PUBLIC_Redis_Required_by_Master
 URL_ARRAY=(${AMQP_URL//:/ })
-echo "${URL_ARRAY[1]}" >> REDIS_IP
+echo "${URL_ARRAY[1]}" > REDIS_IP
